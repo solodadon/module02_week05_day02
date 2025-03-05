98d71bc87c97f4ef569d5adfaaf18e258ddac7ff
solodadon <austinhutchins901@yahoo.com>
2025-03-05 02:09:40 -0600

    fix

--- arrays_lab_2.js
+++ arrays_lab_2.js
@@ -62,6 +62,16 @@ console.log(colors) [blue , green,]
 console.log(numbers)
     // write your code below
 
+    for (let i = 0; i < numbers.length; i++) {
+        let currentNumber = numbers[i];
+        if (currentNumber % 2 === 0) {
+            evenNumbers.push(currentNumber);
+        } else {
+            oddNumbers.push(currentNumber);
+        }
+        numbers.shift();
+        i--;
+    }
 
 
 
